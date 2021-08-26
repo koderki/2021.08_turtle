6bc4b155494ee85c1fcbd274f99e5efaa0c1c8f1
Michal Slaski <michal.slaski@gmail.com>
2021-08-26 14:13:29 +0200

    Rysowanie za pomocą grafiki żółwia

---
+++ drzewo.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from turtle import *
+import time
+
+###############################
+# przepis na rysowanie drzewa #
+###############################
+def drzewo(dlugosc, krok):
+    # warunek zakończenia rekurencji po 8 krokach
+    if krok == 8:
+        return
+
+    # narysowanie pnia
+    forward(dlugosc)
+
+    # rekurencyjne rysowanie kolejnych rozgałęzień,
+    # które z każdym krokiem są o 20% krótsze
+
+    # najpierw skręt w prawo i rysowanie prawej gałęzi
+    right(30)
+    drzewo(0.8 * dlugosc, krok+1)
+
+    # potem skręt w lewo i rysowanie lewej gałęzi
+    left(60)
+    drzewo(0.8 * dlugosc, krok+1)
+
+    # powrót żółwia do miejsca rozwidlenia gałęzi
+    right(30)
+    backward(dlugosc)
+
+
+##########################
+# wykorzystanie przepisu #
+##########################
+
+# ustawienie prędkości żółwia w skali 0-10
+speed(10)
+
+# obrót żółwia tak, aby miał głowę w górę
+# i zaczął od narysowania pionowego pnia
+left(90)
+backward(100)
+
+# rysowanie wg przepisu na drzewo
+# zaczynając od pnia długości 100 i pierwszego kroku
+drzewo(100, 1)
+
+# zaczekaj 10 sekund zanim obraz zgaśnie
+time.sleep(10)
---
+++ kwadrat.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from turtle import *
+import time
+
+#################################
+# przepis na rysowanie kwadratu #
+#################################
+def kwadrat(krok):
+    right(90)      # obróć się na rogu kwadratu o 90 stopni
+    forward(100)   # narysuj bok o długości 100 pikseli
+
+    if krok == 4:  # po narysowaniu kwadratu w 4 krokach
+        return     # zakończ rysowanie
+
+    kwadrat(krok + 1) # wykonaj kolejny krok rekurencji
+
+
+##########################
+# wykorzystanie przepisu #
+##########################
+
+# narysuj kwadrat zaczynając od pierwszego kroku
+kwadrat(1)
+
+# zaczekaj 10 sekund zanim obraz zgaśnie
+time.sleep(10)
